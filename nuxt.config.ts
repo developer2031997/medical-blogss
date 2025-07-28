aa95c4ab4609fe45e744094652a2dc99e6b8a720
Akshay Dhivare <developer2.mtcllp@gmail.com>
2025-07-28 17:53:07 +0530

    chnages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: [
-     // '~~/layout/main.css',
-     '@/assets/css/tailwind.css',
+    // '~~/layout/main.css',
+    "@/assets/css/tailwind.css",
   ],
   postcss: {
     plugins: {
@@ -13,13 +13,13 @@ export default defineNuxtConfig({
   },
   build: {
     transpile: [],
-
   },
   modules: [
-    '@nuxt/content',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/tailwindcss',
-    "@nuxt/image"
+    "@nuxt/content",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@vite-pwa/nuxt",
   ],
   components: true,
   googleFonts: {
@@ -28,8 +28,31 @@ export default defineNuxtConfig({
     },
     // display: 'swap', // Optional: ensures text remains visible during webfont load
   },
-  content: {
+  content: {},
+  plugins: [],
+
+  // ✅ PWA Config
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "My Nuxt Blog",
+      short_name: "Blog",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#00DC82",
+      icons: [
+        // {
+        //   src: "/icon-192.png",
+        //   sizes: "192x192",
+        //   type: "image/png",
+        // },
+        // {
+        //   src: "/icon-512.png",
+        //   sizes: "512x512",
+        //   type: "image/png",
+        // },
+      ],
+    },
   },
-  plugins: [
-  ]
-})
+});
